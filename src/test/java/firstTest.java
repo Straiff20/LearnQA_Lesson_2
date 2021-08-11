e7e2b9eaf558af0b8347402af05be146294af399
Плиев Илья <pliyev.ilya@adv.ru>
2021-08-11 19:43:03 +0300

    Search and Clear

--- src/test/java/firstTest.java
+++ src/test/java/firstTest.java
@@ -19,7 +19,10 @@ public class firstTest {
 
     private static AndroidDriver ad;
 
-    private static final String ERROR_MESSAGE_SEARCH_WIKIPEDIA = "Cannot find text \"Search Wikipedia\"";
+    private static final String ERROR_MESSAGE_SEARCH_WIKIPEDIA = "Cannot find text \"Search Wikipedia\"" + "\n";
+    private static final String ERROR_MESSAGE_NOT_FIND_INPUT = "Cannot find input" + "\n";
+    private static final String ERROR_MESSAGE_NOT_FIND_ARTICLES = "Cannot find articles" + "\n";
+    private static final String ERROR_MESSAGE_NOT_FIND_SEARCH_IMAGE = "Cannot find Search Image" + "\n";
 
     @BeforeAll
     static void setUp() throws MalformedURLException {
@@ -42,7 +45,18 @@ public class firstTest {
     @Test
     void searchTextTest() throws InterruptedException {
         onboardingSkip();
-        assertElementHasTextwaitElement("//*[contains(@text,'Search Wikipedia')]", ERROR_MESSAGE_SEARCH_WIKIPEDIA, 5);
+        waitElement(By.xpath("//*[contains(@text,'Search Wikipedia')]"), ERROR_MESSAGE_SEARCH_WIKIPEDIA, 5);
+    }
+
+    @Test
+    void searchWordCheckAndClear() {
+        onboardingSkip();
+        waitElementAndTap(By.xpath("//*[contains(@text,'Search Wikipedia')]"), ERROR_MESSAGE_SEARCH_WIKIPEDIA);
+        WebElement element = waitElementAndSendKeys(By.id("org.wikipedia:id/search_src_text"), "Appium", ERROR_MESSAGE_NOT_FIND_INPUT);
+        waitElement(By.xpath("//*[contains(@class, 'android.view.ViewGroup')]"), ERROR_MESSAGE_NOT_FIND_ARTICLES);
+        element.clear();
+        waitElement(By.id("org.wikipedia:id/search_empty_image"), ERROR_MESSAGE_NOT_FIND_SEARCH_IMAGE);
+
     }
 
     // Метод для пропуска онбординга при запуске мобильного приложения
@@ -52,10 +66,26 @@ public class firstTest {
         touchAction.tap(PointOption.point(108, 1715)).perform();
     }
 
-    private static WebElement assertElementHasTextwaitElement(String xpath, String errorMessage, long timeInSecond) {
+    private static WebElement waitElement(By by, String errorMessage, long timeInSecond) {
         WebDriverWait wait = new WebDriverWait(ad, timeInSecond);
         wait.withMessage(errorMessage + "\n");
-        By by = By.xpath(xpath);
         return wait.until(ExpectedConditions.presenceOfElementLocated(by));
+
+    }
+
+    private static WebElement waitElement(By by, String errorMessage) {
+        return waitElement(by, errorMessage, 5);
+    }
+
+    private static WebElement waitElementAndTap(By by, String errorMessage) {
+        WebElement element = waitElement(by, errorMessage, 5);
+        element.click();
+        return element;
+    }
+
+    private static WebElement waitElementAndSendKeys(By by, String sendKeys, String errorMessage) {
+        WebElement element = waitElement(by, errorMessage, 5);
+        element.sendKeys(sendKeys);
+        return element;
     }
 }
